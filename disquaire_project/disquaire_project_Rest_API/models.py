19c1688220d774ed240a312e2546637b21e615a6
Ines-Bouguerra <ines.bouguerra2207@gmail.com>
2021-01-09 00:18:16 +0100

    Migrate Data Model to MongoDB database

--- disquaire_project/disquaire_project_Rest_API/models.py
+++ disquaire_project/disquaire_project_Rest_API/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class DisquaireProjectRestApi(models.Model):
+    name = models.CharField(max_length=70, blank=False, default='')
+    owner = models.CharField(max_length=200,blank=False, default='')
+    encoding = models.CharField(max_length=70, blank=False, default='')
+    collate = models.CharField(max_length=70, blank=False, default='')
+    ctype = models.CharField(max_length=70, blank=False, default='')
