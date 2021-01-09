07bf97d095218135b3fca53cf99b26a1807f979c
Ines-Bouguerra <ines.bouguerra2207@gmail.com>
2021-01-09 02:58:43 +0100

    réglages settings.py

--- disquaire_project/disquaire_project/settings.py
+++ disquaire_project/disquaire_project/settings.py
@@ -121,9 +121,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC+01:00'
 
 USE_I18N = True
 
