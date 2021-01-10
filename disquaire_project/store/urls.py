3ff6a474651c33c7220b775652c8d9744a10cf00
Ines-Bouguerra <ines.bouguerra2207@gmail.com>
2021-01-10 13:54:50 +0100

    Lister les albums

--- disquaire_project/store/urls.py
+++ disquaire_project/store/urls.py
@@ -4,5 +4,5 @@ from . import views  # import views so we can use them in urls.
 
 
 urlpatterns = [
-    # "/store" will call the method "index" in "views.py"
-]
+    url(r'^$', views.listing),
+     ]
--- disquaire_project/store/views.py
+++ disquaire_project/store/views.py
@@ -1,8 +1,15 @@
 # Create your views here.
 
 from django.http import HttpResponse
+from .models import ALBUMS
 
 
 def index(request):
     message = "Salut tout le monde !"
+    return HttpResponse(message)
+
+
+def listing(request):
+    albums = ["<li>{}</li>".format(album['name']) for album in ALBUMS]
+    message = """<ul>{}</ul>""".format("\n".join(albums))
     return HttpResponse(message)
