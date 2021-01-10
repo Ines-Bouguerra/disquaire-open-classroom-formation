7f86a1beee389c65051fbfdd013ab788f5a88afd
Ines-Bouguerra <ines.bouguerra2207@gmail.com>
2021-01-10 19:12:14 +0100

    Ajout des trois modèles

--- disquaire_project/store/models.py
+++ disquaire_project/store/models.py
@@ -21,3 +21,21 @@ ALBUMS = [
 
 class Artist(models.Model):
     name = models.CharField(max_length=200, unique=True)
+
+
+class Contact(models.Model):
+    email = models.EmailField(max_length=100)
+    name = models.CharField(max_length=200)
+
+
+class Album(models.Model):
+    reference = models.IntegerField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    available = models.BooleanField(default=True)
+    title = models.CharField(max_length=200)
+    picture = models.URLField()
+
+
+class Booking(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    contacted = models.BooleanField(default=False)
